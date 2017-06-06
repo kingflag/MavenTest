2743dd39b511eceee911a7e7edc7abe886bc5331
kingflag <1362168862@qq.com>
2017-06-06 18:51:03 +0800

    master

--- wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/core/job/WriteJob.java
+++ wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/core/job/WriteJob.java
@@ -6,11 +6,15 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import com.hd123.h4.wangguoqiTest.controller.WriteJobController;
+
 public class WriteJob implements ApplicationContextAware {
 
   protected static final Logger logger = LoggerFactory.getLogger(WriteJob.class);
   private ApplicationContext appCtx;
 
+  private WriteJobController writeJobControllerImpl;
+  
   @Override
   public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
     this.appCtx = applicationContext;
@@ -21,7 +25,7 @@ public class WriteJob implements ApplicationContextAware {
     String beanName = "writeJobcontroller";
     if (appCtx.containsBean(beanName)) {
       System.out.println("存在WriteJobcontroller");
-      
+      //writeJobControllerImpl.insert();
     }else {
       System.out.println("不存在此bean");
     }
