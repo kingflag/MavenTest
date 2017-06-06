31728313131a505ddebffa830880fc082e560381
kingflag <1362168862@qq.com>
2017-06-06 18:28:41 +0800

    666

--- wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/controller/WriteJobControllerImpl.java
+++ wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/controller/WriteJobControllerImpl.java
@@ -1,30 +1,30 @@
 package com.hd123.h4.wangguoqiTest.controller;
 
-import org.springframework.stereotype.Controller;
-
 import com.hd123.h4.wangguoqiTest.service.WriteJobService;
 
-
-
-@Controller("writeJobcontroller")
+/**
+ * 
+ * @author wangguoqi
+ *
+ */
 public class WriteJobControllerImpl implements WriteJobController {
   
   
 
-//  WriteJobService writeJobServiceImpl;
-//
-//  public WriteJobService getWriteJobServiceImpl() {
-//    return writeJobServiceImpl;
-//  }
-//
-//  public void setWriteJobServiceImpl(WriteJobService writeJobServiceImpl) {
-//    this.writeJobServiceImpl = writeJobServiceImpl;
-//  }
+  private WriteJobService writeJobServiceImpl;
+
+  public WriteJobService getWriteJobServiceImpl() {
+    return writeJobServiceImpl;
+  }
+
+  public void setWriteJobServiceImpl(WriteJobService writeJobServiceImpl) {
+    this.writeJobServiceImpl = writeJobServiceImpl;
+  }
 
   @Override
   public void insert() {
     System.out.println("插入过程");
-    //writeJobServiceImpl.insert();
+    writeJobServiceImpl.insert();
   }
 
 }
