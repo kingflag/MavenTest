1602762c48cfa1f951b65a8d1b3c861612be41ca
kingflag <1362168862@qq.com>
2017-06-07 10:16:00 +0800

    ...

--- wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/dao/WriteJobDao.java
+++ wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/dao/WriteJobDao.java
@@ -1,5 +1,6 @@
 package com.hd123.h4.wangguoqiTest.dao;
 
 public interface WriteJobDao {
+  public String insert();
 
 }
--- wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/service/WriteJobServiceImpl.java
+++ wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/service/WriteJobServiceImpl.java
@@ -1,11 +1,18 @@
 package com.hd123.h4.wangguoqiTest.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hd123.h4.wangguoqiTest.dao.WriteJobDao;
+
 
 public class WriteJobServiceImpl implements WriteJobService {
 
+  @Autowired
+  WriteJobDao writeJobDaoImpl;  
+  
   @Override
   public String insert() {
-    System.out.println("ServiceTestinsert");
+    writeJobDaoImpl.insert();
     return "ServiceTestinsert";
   }
 
