4cc1e3aeb6f297c96271fd5f04f39ed76880fa1b
kingflag <1362168862@qq.com>
2017-06-07 11:59:52 +0800

    【kingfalg】back-up_20170607115952

--- wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/dao/WriteJobDaoImpl.java
+++ wangguoqiTest-core/src/main/java/com/hd123/h4/wangguoqiTest/dao/WriteJobDaoImpl.java
@@ -1,5 +1,7 @@
 package com.hd123.h4.wangguoqiTest.dao;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
 
@@ -10,29 +12,39 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class WriteJobDaoImpl implements WriteJobDao {
 
   private JdbcTemplate jdbcTemplate;
-  
+
   public void setDataSource(DataSource dataSource) {
-      this.jdbcTemplate = new JdbcTemplate(dataSource);
+    this.jdbcTemplate = new JdbcTemplate(dataSource);
   }
-  
+
   @Override
-  public String insert() throws Exception  {
+  public String insert() throws Exception {
     System.out.println("daoc============+++++++");
 
-    /*测试数据库连接信息*/
-//    DatabaseMetaData md = this.jdbcTemplate.getDataSource().getConnection().getMetaData();  
-//    System.out.println(md.getDatabaseProductName());  
-//    System.out.println(md.getDatabaseProductVersion()); 
+    /* 测试数据库连接信息 */
+    // DatabaseMetaData md =
+    // this.jdbcTemplate.getDataSource().getConnection().getMetaData();
+    // System.out.println(md.getDatabaseProductName());
+    // System.out.println(md.getDatabaseProductVersion());
     UUID uuid = UUID.randomUUID();
-    
-   
+
     Random r = new Random();
     float surplus = r.nextInt(12365);
+
     String sql = "insert into hduserinfo (user,surplus,createtime) values (uuid,'65.6','2017-06-21 11:02:59')";
     System.out.println(sql);
-    int temp = jdbcTemplate.update("insert into hduserinfo (user,surplus,createtime) values ('"+uuid+"','"+surplus+"','2017-06-21 11:02:59')");
-    System.out.println("插入成功："+String.valueOf(temp));
+    int temp = jdbcTemplate.update("insert into hduserinfo (user,surplus,createtime) values ('"
+        + uuid + "','" + surplus + "','" + dateToString(new Date()) + "')");
+    System.out.println("插入成功：" + String.valueOf(temp));
     return null;
+  }
+
+  private static String dateToString(Date time) {
+    SimpleDateFormat formatter;
+    formatter = new SimpleDateFormat("yyyy-MM-dd KK:mm:ss");
+    String ctime = formatter.format(time);
+
+    return ctime;
   }
 
 }
